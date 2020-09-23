d85d759860643be76cb56b8b3c73e8d92fc032c1
Fabian <19959@ma-web.nl>
2020-09-23 12:26:23 +0200

    footer toegevoegt

--- products.php
+++ products.php
@@ -43,7 +43,7 @@ $statement = getProducts($connection);
     <div class="products--product-container">
         <?php foreach ($statement as $product): ?>
             <div class="product-container">
-                <img src="images/<?php echo $product['image'] ?>" alt="" class="product-image">
+                <img src="images/imgIndex/<?php echo $product['image'] ?>" alt="" class="product-image">
                 <h2 class="product-name"><?php echo $product['product_name'] ?></h2>
                 <p class="product-description"><?php echo $product['description'] ?></p>
                 <a href="portfolio.php" class="product__more-button">Lees meer</a>
@@ -52,6 +52,53 @@ $statement = getProducts($connection);
         <?php endforeach; ?>
     </div>
 </div>
+<footer class="kleur1">
+    <section class="footer--Positionering">
+
+        <article class="footerItems artClassSec">
+            <a class="footerItems" href="#">Over ons</a>
+
+            <a class="footerItems" href="#">Wij van Luso stellen <br>
+                communicatie <br>
+                boven alles.<br>
+                Wij nemen u mee in <br>
+                het ontwerp proces <br>
+                en zorgen voor een <br>
+                luxe uitkomst, waar <br>u achter staat</a>
+
+        </article>
+
+        <p class="footerRuimte"></p>
+
+        <article class="footerItems artClassSec artClassSec2">
+            <a class="footerItems" href="#">Links</a>
+            <a class="footerItems2" href="http://firesofdestruction.nl/idp/online/index.html">Home</a>
+            <a class="footerItems2" href="#">Over ons</a>
+            <a class="footerItems2" href="#">Diensten</a>
+            <a class="footerItems2" href="#">Portfolio</a>
+            <a class="footerItems2" href="http://firesofdestruction.nl/idp/online/contact.html">Contact</a>
+        </article>
+
+        <p class="footerRuimte"></p>
+
+
+        <article class="footerItems artClassSec artClassSec3">
+            <a class="footerItems" href="http://firesofdestruction.nl/idp/online/contact.html">Contact</a>
+            <a class="footerItems" href="#">luso.design@hotmail.com</a>
+            <a class="footerItems" href="#">06 12 34 56 78</a>
+
+            <div class="">
+                <a class="footerItemsImg" href=""><img src="./images/social/linkedin.png" alt="Linkedin"></a>
+                <a class="footerItemsImg" href=""><img src="./images/social/instagram.png" alt="Instagram"></a>
+                <a class="footerItemsImg" href=""><img src="./images/social/facebook.png" alt="Facebook"
+                                                       style="width: 15px"></a>
+            </div>
+            <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" style="width: 85px"
+                     alt="Valide CSS!"></a>
+        </article>
+    </section>
+</footer>
 </body>
 </html>
 
