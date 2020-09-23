155246056c1c00b7ea15ffcdd711b41e4ba6b04b
Fabian <19959@ma-web.nl>
2020-09-23 11:58:22 +0200

    image links aangepast

--- portfolio.php
+++ portfolio.php
@@ -45,7 +45,7 @@
                 niet zijn originele nummer 23. Dit was een verwijzing naar het doelnummer 64. Verder hadden we de tekst
                 erbij gezet ‘be the best, become the biggest’.
             </div>
-            <img src="images/Mockup-POster-sam.jpg" alt="" class="work-item__image">
+            <img src="images/imgIndex/Mockup-POster-sam.jpg" alt="" class="work-item__image">
         </div>
         <div class="work-item work-item__main work-item__right">
             <h2 class="work-item-head">Logo planet visum</h2>
@@ -55,7 +55,7 @@
                 bedrijf vond dit staan voor vrijheid. De klant was erg blij met het logo en heeft nu een prachtig logo
                 die zichtbaar is op al het bedrijf hun producten.
             </div>
-            <img src="images/Mockup-logo-ferdy.jpg" alt="" class="work-item__image">
+            <img src="images/imgIndex/Mockup-logo-ferdy.jpg" alt="" class="work-item__image">
         </div>
 
         <div class="work-item work-item__main-light">
@@ -65,13 +65,59 @@
                 hebben wij een strakke poster template neer gezet die makkelijk te bewerken is en handig is voor alle
                 opkomende evenementen van het bedrijf
             </div>
-            <img src="images/Mockup-poster-ferdy.jpg" alt="" class="work-item__image">
+            <img src="images/imgIndex/Mockup-poster-ferdy.jpg" alt="" class="work-item__image">
         </div>
 
     </div>
 
 
 </div>
+<footer class="kleur1">
+    <section class="footer--Positionering">
 
+        <article class="footerItems artClassSec">
+            <a class="footerItems" href="#">Over ons</a>
+
+            <a class="footerItems" href="#">Wij van Luso stellen <br>
+                communicatie <br>
+                boven alles.<br>
+                Wij nemen u mee in <br>
+                het ontwerp proces <br>
+                en zorgen voor een <br>
+                luxe uitkomst, waar <br>u achter staat</a>
+
+        </article>
+
+        <p class="footerRuimte"></p>
+
+        <article class="footerItems artClassSec artClassSec2">
+            <a class="footerItems" href="#">Links</a>
+            <a class="footerItems2" href="http://firesofdestruction.nl/idp/online/index.html">Home</a>
+            <a class="footerItems2" href="#">Over ons</a>
+            <a class="footerItems2" href="#">Diensten</a>
+            <a class="footerItems2" href="#">Portfolio</a>
+            <a class="footerItems2" href="http://firesofdestruction.nl/idp/online/contact.html">Contact</a>
+        </article>
+
+        <p class="footerRuimte"></p>
+
+
+        <article class="footerItems artClassSec artClassSec3">
+            <a class="footerItems" href="http://firesofdestruction.nl/idp/online/contact.html">Contact</a>
+            <a class="footerItems" href="#">luso.design@hotmail.com</a>
+            <a class="footerItems" href="#">06 12 34 56 78</a>
+
+            <div class="">
+                <a class="footerItemsImg" href=""><img src="./images/social/linkedin.png" alt="Linkedin"></a>
+                <a class="footerItemsImg" href=""><img src="./images/social/instagram.png" alt="Instagram"></a>
+                <a class="footerItemsImg" href=""><img src="./images/social/facebook.png" alt="Facebook"
+                                                       style="width: 15px"></a>
+            </div>
+            <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" style="width: 85px"
+                     alt="Valide CSS!"></a>
+        </article>
+    </section>
+</footer>
 </body>
 </html>
